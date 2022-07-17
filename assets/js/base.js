8690a08152f829a5c9b0bc51c6e5bb6baadaecfb
Beatriz Beato <beatrizbeatosilva@gmail.com>
2022-07-17 20:32:19 +0100

    first design iteration (temp)

--- assets/js/base.js
+++ assets/js/base.js
@@ -36,6 +36,35 @@ $('a[href*="#"]')
       }
     }
   });
+
+  var offset = $(".moving").offset();
+  console.log(offset.left);
+  $(window).scroll(function(event) {
+    var st = $(this).scrollTop();
+    console.log(st);
+    if (st > 700 && st <= 1000)  {
+      $(".moving").css("left", offset.left - (st-699));
+    } 
+    if (st > 1000 && st <= 1400){
+      $(".moving").css("left", offset.left + (st-999));
+    }if (st > 1400 && st <= 1800){
+      $(".moving").css("left", offset.left - (st-1399));
+    }if (st > 1800 && st <= 2200){
+      $(".moving").css("left", offset.left + (st-1799));
+    }if (st > 2200 && st <= 2600){
+      $(".moving").css("left", offset.left - (st-2199));
+    }if (st > 2600 && st <= 3000){
+      $(".moving").css("left", offset.left + (st-2599));
+    }if (st > 3000 && st <= 3400){
+      $(".moving").css("left", offset.left - (st-2999));
+    }if (st > 3400 && st <= 3800){
+      $(".moving").css("left", offset.left + (st-3399));
+    }if (st > 3800 && st <= 4200){
+      $(".moving").css("left", offset.left - (st-3799));
+    }if (st > 4200 && st <= 4600){
+      $(".moving").css("left", offset.left - (st-4199));
+    }
+  });
 });
 // ---
 
